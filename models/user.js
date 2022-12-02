c7a2e077f7d15154ddb9e41ef549e33341441179
Mario Delgado <mdelgado1138@hotmail.com>
2022-12-01 16:31:23 -0800

    update

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+// const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  products: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Product' }],
+});
+
+// temporarily commented out; produces error: User validation failed: _id: Error, expected `_id` to be unique
+// when calling deletePlace in places-controller
+// userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
