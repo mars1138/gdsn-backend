dc2b81a48610de6730e0f9004f30c51da7cabbc1
Mario Delgado <mdelgado1138@hotmail.com>
2022-12-07 16:11:47 -0800

    update product

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -15,6 +15,7 @@ const getProductById = async (req, res, next) => {
 
   try {
     product = await Product.find({ gtin: prodId });
+    console.log('loaded product: ', product);
   } catch (err) {
     const error = new HttpError(
       'Something went wrong, could not find product',
@@ -32,7 +33,7 @@ const getProductById = async (req, res, next) => {
     return next(error);
   }
 
-  res.json({ message: 'get request received!', product });
+  res.json({ product: product[0].toObject({ getters: true }) });
 };
 
 const getProductsByUserId = async (req, res, next) => {};
@@ -112,15 +113,105 @@ const createProduct = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({ product: createdProd });
+  res.status(201).json({ product: createdProd.toObject({ getters: true }) });
 };
 
-const updateProduct = async (req, res, next) => {};
+const updateProduct = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError('Invalid inputs passed, please check your data', 422),
+    );
+  }
+
+  const {
+    name,
+    description,
+    // gtin,
+    category,
+    // type,
+    image,
+    height,
+    width,
+    depth,
+    weight,
+    packagingType,
+    tempUnits,
+    minTemp,
+    maxTemp,
+    storageInstructions,
+    subscribers,
+    // dateAdded,
+    datePublished,
+    dateInactive,
+    dateModified,
+  } = req.body;
+
+  const prodId = req.params.pid;
+
+  let product;
+
+  try {
+    product = await Product.find({ gtin: prodId });
+    console.log('loaded product: ', product);
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not update product',
+      500,
+    );
+
+    return next(error);
+  }
+
+  // if (product.owner.toString() !== req.userData.userId) {
+  //   const error = new HttpError(
+  //     'You are not authorized to edit this place.',
+  //     401,
+  //   );
+  //   return next(error);
+  // }
+
+  product[0].name = name;
+  product[0].description = description;
+  // gtin,
+  product[0].category = category;
+  // type,
+  product[0].image = image;
+  product[0].height = height;
+  product[0].width = width;
+  product[0].depth = depth;
+  product[0].weight = weight;
+  product[0].packagingType = packagingType;
+  product[0].tempUnits = tempUnits;
+  product[0].minTemp = minTemp;
+  product[0].maxTemp = maxTemp;
+  product[0].storageInstructions = storageInstructions;
+  product[0].subscribers = subscribers;
+  // dateAdded,
+  product[0].datePublished = datePublished;
+  product[0].dateInactive = dateInactive;
+  product[0].dateModified = dateModified;
+
+  console.log('product: ', product);
+
+  try {
+    await product.forEach(item => item.save());
+  } catch (err) {
+    console.log(err);
+    const error = new HttpError(
+      'Something went wrong, could not update place',
+      500,
+    );
+    return next(error);
+  }
+
+  res.status(200).json({ product: product[0].toObject({ getters: true }) });
+};
 
 const deleteProduct = async (req, res, next) => {};
 
 exports.getProductById = getProductById;
 exports.getProductsByUserId = getProductsByUserId;
 exports.createProduct = createProduct;
-exports.updateProduct = createProduct;
+exports.updateProduct = updateProduct;
 exports.deleteProduct = deleteProduct;
--- routes/products-routes.js
+++ routes/products-routes.js
@@ -21,13 +21,36 @@ router.post(
     check('description').isLength({ min: 10 }),
     check('gtin').isLength({ min: 14, max: 14 }),
     check('category').isNumeric(),
+    check('type').isNumeric(),
+    check('height').isNumeric(),
+    check('width').isNumeric(),
+    check('depth').isNumeric(),
+    check('weight').isNumeric(),
+    check('packagingType').isNumeric(),
+    check('tempUnits').isNumeric(),
+    check('minTemp').isNumeric(),
+    check('maxTemp').isNumeric(),
   ],
   productsControllers.createProduct,
 );
 
 router.patch(
   '/:pid',
-  [check('name').not().isEmpty(), check('description').isLength({ min: 10 })],
+  [
+    check('name').not().isEmpty(),
+    check('description').isLength({ min: 10 }),
+    check('gtin').isLength({ min: 14, max: 14 }),
+    check('category').isNumeric(),
+    check('type').isNumeric(),
+    check('height').isNumeric(),
+    check('width').isNumeric(),
+    check('depth').isNumeric(),
+    check('weight').isNumeric(),
+    check('packagingType').isNumeric(),
+    check('tempUnits').isNumeric(),
+    check('minTemp').isNumeric(),
+    check('maxTemp').isNumeric(),
+  ],
   productsControllers.updateProduct,
 );
 
